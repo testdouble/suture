b0af8284f6dc206a53f684880d8d4f55fd9b9a0c
Justin Searls <searls@gmail.com>
2016-08-21 20:12:28 -0400

    maybe fix for 1.8.7

--- lib/suture/surgeon/observer.rb
+++ lib/suture/surgeon/observer.rb
@@ -4,8 +4,18 @@ module Suture::Surgeon
   class Observer
     def operate(plan)
       dictaphone = Suture::Adapter::Dictaphone.new(plan)
-      plan.old.call(*plan.args).tap do |result|
+      invoke(plan).tap do |result|
         dictaphone.record(result)
+      end
+    end
+
+    private
+
+    def invoke(plan)
+      if plan.args
+        plan.old.call(*plan.args)
+      else
+        plan.old.call
       end
     end
   end
