9763899f5e2c5cf67987d9c4af6f9f1e6947a420
Justin Searls <searls@gmail.com>
2016-08-23 09:40:51 -0400

    Address issues in sqlite wrap

--- safe/sqlite_wrap_test.rb
+++ safe/sqlite_wrap_test.rb
@@ -5,37 +5,33 @@ class SqliteWrapTest < SafeTest
   DB_PATH = "db/lol.db"
 
   def test_can_deal_with_multiple_loads
-    db = Suture::Wrap::Sqlite.init(DB_PATH)
+    Suture::Wrap::Sqlite.init(DB_PATH)
 
     # is happy
-    db = Suture::Wrap::Sqlite.init(DB_PATH)
+    Suture::Wrap::Sqlite.init(DB_PATH)
   end
 
   def test_major_version_newer
-    db = Suture::Wrap::Sqlite.init(DB_PATH)
+    Suture::Wrap::Sqlite.init(DB_PATH)
 
     expected_error = nil
     Suture::Wrap::Sqlite.send(:remove_const, "SCHEMA_VERSION")
     Suture::Wrap::Sqlite.const_set("SCHEMA_VERSION", 992)
-    begin
+    expected_error = assert_raises(Suture::Error::SchemaVersion) {
       Suture::Wrap::Sqlite.init(DB_PATH)
-    rescue Suture::Error::SchemaVersion => e
-      expected_error = e
-    end
+    }
     assert_equal "Your suture gem is too new for this schema. Either delete your database or upgrade the gem (expected schema version 992, was #{ACTUAL})", expected_error.message
   end
 
   def test_major_version_older
-    db = Suture::Wrap::Sqlite.init(DB_PATH)
+    Suture::Wrap::Sqlite.init(DB_PATH)
 
     expected_error = nil
     Suture::Wrap::Sqlite.send(:remove_const, "SCHEMA_VERSION")
     Suture::Wrap::Sqlite.const_set("SCHEMA_VERSION", -44)
-    begin
+    expected_error = assert_raises(Suture::Error::SchemaVersion) {
       Suture::Wrap::Sqlite.init(DB_PATH)
-    rescue Suture::Error::SchemaVersion => e
-      expected_error = e
-    end
+    }
     assert_equal "Your suture gem is too old for this schema. Either delete your database or downgrade the gem (expected schema version -44, was #{ACTUAL})", expected_error.message
   end
 
