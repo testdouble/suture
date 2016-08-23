3f78b2c65e77706fd3ce9deb1d68370a70472c97
Justin Searls <searls@gmail.com>
2016-08-23 10:23:20 -0400

    ignore some stuff that isn't coverable/intended

--- safe/support/code_climate.rb
+++ safe/support/code_climate.rb
@@ -1,5 +1,8 @@
 if Gem.ruby_version >= Gem::Version.new("1.9.3")
   require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  CodeClimate::TestReporter.start do
+    add_filter "/safe/fixtures" #<- not intended to be covered w/ tests.
+    add_filter "/lib/suture/wrap/logger.rb" #<- can't test STDOUT easily
+  end
 end
 
