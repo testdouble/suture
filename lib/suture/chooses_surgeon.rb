ede2b25a83967db8e096f37ebbd3d6faecaf9a21
Justin Searls <searls@gmail.com>
2016-08-23 10:26:25 -0400

    Whoops, committed too soon

--- lib/suture/chooses_surgeon.rb
+++ lib/suture/chooses_surgeon.rb
@@ -1,16 +1,20 @@
+require "suture/adapter/log"
 require "suture/surgeon/observer"
 require "suture/surgeon/no_op"
 
 module Suture
   class ChoosesSurgeon
+    include Suture::Adapter::Log
+
     def choose(plan)
       if plan.record_calls
         if plan.new
-          log_warn <<-MSG
-            Seam #{name.inspect} has a :new code path defined, but because it is
-            set to :record_calls, we will invoke the :old code path instead. If
-            this is not what you intend, set :record_calls to false.
+          log_warn <<-MSG.gsub(/^ {12}/,'')
+            Seam #{plan.name.inspect} has a :new code path defined, but because
+            it is set to :record_calls, we will invoke the :old code path
+            instead. If this is not what you intend, set :record_calls to false.
           MSG
+        end
         Surgeon::Observer.new
       else
         Surgeon::NoOp.new
