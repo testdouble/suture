1b680a9d53a8c123024805d733b9325bc5bb6e0d
Justin Searls <searls@gmail.com>
2016-08-23 11:22:27 -0400

    disable coverage locally

--- safe/support/code_climate.rb
+++ safe/support/code_climate.rb
@@ -1,4 +1,4 @@
-if Gem.ruby_version >= Gem::Version.new("1.9.3")
+if Gem.ruby_version >= Gem::Version.new("1.9.3") && ENV['CI']
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start do
     add_filter "/safe/fixtures" #<- not intended to be covered w/ tests.
