fe6ac1f57d290585dcc586f0075d1fccaecd3156
Justin Searls <searls@gmail.com>
2016-08-23 13:00:50 -0500

    whoops my filters were on the wrong block

--- safe/support/code_climate.rb
+++ safe/support/code_climate.rb
@@ -1,8 +1,9 @@
 if Gem.ruby_version >= Gem::Version.new("1.9.3") && ENV['CI']
   require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start do
+  SimpleCov.start do
     add_filter "/safe/fixtures" #<- not intended to be covered w/ tests.
     add_filter "/lib/suture/wrap/logger.rb" #<- can't test STDOUT easily
   end
+  CodeClimate::TestReporter.start
 end
 
