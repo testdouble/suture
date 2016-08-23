c1eacc15287d648bc0f034cedca7dc4790a7f174
Justin Searls <searls@gmail.com>
2016-08-23 10:23:10 -0400

    Add a warning that we're invoking old when record

--- lib/suture/chooses_surgeon.rb
+++ lib/suture/chooses_surgeon.rb
@@ -5,6 +5,12 @@ module Suture
   class ChoosesSurgeon
     def choose(plan)
       if plan.record_calls
+        if plan.new
+          log_warn <<-MSG
+            Seam #{name.inspect} has a :new code path defined, but because it is
+            set to :record_calls, we will invoke the :old code path instead. If
+            this is not what you intend, set :record_calls to false.
+          MSG
         Surgeon::Observer.new
       else
         Surgeon::NoOp.new
