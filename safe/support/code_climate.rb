5e4224672e413f34feffe261339b11eb1527bc50
Justin Searls <searls@gmail.com>
2016-08-29 12:23:01 -0400

    Now that #50 is fixed, this is fair coverage game

--- safe/support/code_climate.rb
+++ safe/support/code_climate.rb
@@ -2,7 +2,6 @@ if Gem.ruby_version >= Gem::Version.new("1.9.3") && ENV['CI']
   require "codeclimate-test-reporter"
   SimpleCov.start do
     add_filter "/safe/fixtures" #<- not intended to be covered w/ tests.
-    add_filter "/lib/suture/wrap/logger.rb" #<- can't test STDOUT easily
   end
   CodeClimate::TestReporter.start
 end
