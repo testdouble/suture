15a3709d5c4f06bc877a72ded128706a02bcdd2f
Justin Searls <searls@gmail.com>
2016-08-29 12:21:32 -0400

    log (unexpected) errors when invoking anything.
    Fixes #50

--- lib/suture/util/scalpel.rb
+++ lib/suture/util/scalpel.rb
@@ -1,5 +1,9 @@
+require "suture/adapter/log"
+
 module Suture::Util
   class Scalpel
+    include Suture::Adapter::Log
+
     def cut(plan, location, args_override = nil)
       args = args(plan, args_override)
       begin
@@ -7,6 +11,7 @@ module Suture::Util
           call_after_hook(plan, location, args, result)
         end
       rescue StandardError => error
+        log_error_details(plan, location, args, error)
         call_error_hook(plan, location, args, error)
         raise error
       end
@@ -19,10 +24,26 @@ module Suture::Util
       after_hook.call(plan.name, args, result)
     end
 
+    def log_error_details(plan, location, args, error)
+      return if expected_error?(plan, error)
+      log_error <<-MSG.gsub(/^ {8}/,'')
+        Suture invoked the #{plan.name.inspect} seam's #{location.inspect} code path with args: ```
+          #{args.inspect}
+        ```
+        which raised a #{error.class} with message: ```
+          #{error.message}
+        ```
+      MSG
+    end
+
     def call_error_hook(plan, location, args, error)
-      return if plan.expected_error_types.any? {|e| error.kind_of?(e) }
+      return if expected_error?(plan, error)
       return unless error_hook = try(plan, "on_#{location}_error")
       error_hook.call(plan.name, args, error)
+    end
+
+    def expected_error?(plan, error)
+      plan.expected_error_types.any? {|e| error.kind_of?(e) }
     end
 
     def args(plan, args_override)
--- test/suture/util/scalpel_test.rb
+++ test/suture/util/scalpel_test.rb
@@ -67,17 +67,51 @@ module Suture::Util
     end
 
     def test_doesnt_invoke_on_new_error_when_expected_error
-      on_new_error_called = false
+      on_old_error_called = false
       plan = Suture::BuildsPlan.new.build(:my_seam,
         :old => lambda { raise ZeroDivisionError.new },
-        :on_old_error => lambda { |*args| on_new_error_called = true },
+        :on_old_error => lambda { |*args| on_old_error_called = true },
         :args => [],
         :expected_error_types => [ZeroDivisionError]
       )
 
       assert_raises(ZeroDivisionError) { @subject.cut(plan, :old) }
+      assert_equal false, on_old_error_called
+    end
 
-      assert_equal false, on_new_error_called
+    def test_log_errors_when_unexpected
+      Suture.config(:log_io => log_io = StringIO.new)
+      Suture::Adapter::Log.reset!
+
+      plan = Suture::BuildsPlan.new.build(:my_seam,
+        :old => lambda { |a,b| 100 / 0 },
+        :args => [1, 2]
+      )
+
+      assert_raises(ZeroDivisionError) { @subject.cut(plan, :old) }
+      expected_message = <<-MSG.gsub(/^ {8}/,'')
+        Suture invoked the :my_seam seam's :old code path with args: ```
+          [1, 2]
+        ```
+        which raised a ZeroDivisionError with message: ```
+          divided by 0
+        ```
+      MSG
+      assert_spacey_match expected_message, log_io.tap(&:rewind).read
+    end
+
+    def test_does_not_log_errors_when_expected
+      Suture.config(:log_io => log_io = StringIO.new)
+      Suture::Adapter::Log.reset!
+
+      plan = Suture::BuildsPlan.new.build(:my_seam,
+        :old => lambda { |a,b| 100 / 0 },
+        :args => [1, 2],
+        :expected_error_types => [ZeroDivisionError]
+      )
+
+      assert_raises(ZeroDivisionError) { @subject.cut(plan, :old) }
+      assert_equal "", log_io.tap(&:rewind).read
     end
   end
 end
