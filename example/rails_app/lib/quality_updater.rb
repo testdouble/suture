82618a70d2b7fa86f2352829c7ece597a1c08074
Justin Searls <searls@gmail.com>
2016-09-06 18:33:20 +0900

    Use the recordings to test the new solution
    * Add the new solution to lib/quality_updater.rb
    
    * Extract the custom comparator to be reused
    
    * Write a new test against the new code, which is in fact passing

---
+++ example/rails_app/lib/quality_updater.rb
@@ -0,0 +1,89 @@
+class QualityUpdater
+  def update(item)
+    updater_for(item).update(item)
+  end
+
+  private
+
+  class StandardQualityUpdater
+    def update(item)
+      update_quality(item)
+      update_sell_in(item)
+    end
+
+    def update_quality(item)
+      if item.sell_in <= 0
+        bump(item, -2)
+      else
+        bump(item, -1)
+      end
+    end
+
+    def update_sell_in(item)
+      item.sell_in -= 1
+    end
+
+    def bump(item, amount)
+      item.quality += amount
+      item.quality = 50 if item.quality > 50
+      item.quality = 0 if item.quality < 0
+    end
+  end
+
+  class NoopQualityUpdater < StandardQualityUpdater
+    def update_quality(item)
+    end
+    def update_sell_in(item)
+    end
+  end
+
+  class BrieQualityUpdater < StandardQualityUpdater
+    def update_quality(item)
+      if item.sell_in <= 0
+        bump(item, 2)
+      else
+        bump(item, 1)
+      end
+    end
+  end
+
+  class BackstagePassQualityUpdater < StandardQualityUpdater
+    def update_quality(item)
+      if item.sell_in > 10
+        bump(item, 1)
+      elsif item.sell_in > 5
+        bump(item, 2)
+      elsif item.sell_in > 0
+        bump(item, 3)
+      else
+        item.quality = 0
+      end
+    end
+  end
+
+  class ConjuredItemQualityUpdater < StandardQualityUpdater
+    def update_quality(item)
+      if item.sell_in <= 0
+        bump(item, -4)
+      else
+        bump(item, -2)
+      end
+    end
+  end
+
+  UPDATERS = [
+    [/^Sulfuras, Hand of Ragnaros$/, NoopQualityUpdater.new],
+    [/^Aged Brie$/, BrieQualityUpdater.new],
+    [/^Backstage passes to a TAFKAL80ETC concert$/, BackstagePassQualityUpdater.new],
+    [/^Conjured /, ConjuredItemQualityUpdater.new],
+  ]
+
+  def updater_for(item)
+    pair = UPDATERS.detect { |re, handler| re =~ item.name }
+    handler = pair ? pair[1] : standard_updater
+  end
+
+  def standard_updater
+    @standard_handler ||= StandardQualityUpdater.new
+  end
+end
--- example/rails_app/test/suture/update_quality_test.rb
+++ example/rails_app/test/suture/update_quality_test.rb
@@ -3,6 +3,7 @@ require "suture"
 
 require "application"
 require "models/item"
+require "quality_updater"
 
 class UpdateQualityTest < Minitest::Test
   def setup
@@ -12,15 +13,29 @@ class UpdateQualityTest < Minitest::Test
     )
   end
 
+  class ItemComparator < Suture::Comparator
+    def call(recorded, actual)
+      recorded.attributes.except("created_at", "updated_at") ==
+        actual.attributes.except("created_at", "updated_at")
+    end
+  end
+
   def test_old_update_quality_recordings
     Suture.verify :gilded_rose,
       :subject => lambda { |item|
         item.update_quality!
         item
       },
-      :comparator => lambda { |recorded, actual|
-        recorded.attributes.except("created_at", "updated_at") ==
-          actual.attributes.except("created_at", "updated_at")
-      }
+      :comparator => ItemComparator.new
+  end
+
+  def test_new_update_quality_against_oldrecordings
+    quality_updater = QualityUpdater.new
+    Suture.verify :gilded_rose,
+      :subject => lambda { |item|
+        quality_updater.update(item)
+        item
+      },
+      :comparator => ItemComparator.new
   end
 end
