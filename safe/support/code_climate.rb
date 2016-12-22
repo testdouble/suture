d8df3a5f96b4af6c1b57a1a696bf341a75e450fe
Justin Searls <searls@gmail.com>
2016-12-21 23:16:07 -0500

    try to make codeclimate happy

--- safe/support/code_climate.rb
+++ safe/support/code_climate.rb
@@ -1,8 +1,7 @@
 if Gem.ruby_version >= Gem::Version.new("1.9.3") && ENV['CI']
-  require "codeclimate-test-reporter"
+  require 'simplecov'
   SimpleCov.start do
     add_filter "/safe/fixtures" #<- not intended to be covered w/ tests.
   end
-  CodeClimate::TestReporter.start
 end
 
